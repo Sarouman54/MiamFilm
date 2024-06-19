fc8b50addd0a3f3a38cef015c762761224b2ef7e
MbMyriam <myriam.mb@ynov.com>
2024-06-19 11:17:54 +0200

    Créations des components pour le module Profile

--- miamfilm_front/src/app/component/profile/profile.module.ts
+++ miamfilm_front/src/app/component/profile/profile.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProfileComponent } from './profile/profile.component';
+import { UserUpdateComponent } from './user-update/user-update.component';
+import { UserDeleteComponent } from './user-delete/user-delete.component';
+import { UserPasswordUpdateComponent } from './user-password-update/user-password-update.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProfileComponent,
+    UserUpdateComponent,
+    UserDeleteComponent,
+    UserPasswordUpdateComponent
+  ],
   imports: [
     CommonModule
   ]
---
+++ miamfilm_front/src/app/component/profile/user-password-update/user-password-update.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-password-update',
+  templateUrl: './user-password-update.component.html',
+  styleUrl: './user-password-update.component.scss'
+})
+export class UserPasswordUpdateComponent {
+
+}

e87744051934ecff28895dac4d96323b4e337ebe
MbMyriam <myriam.mb@ynov.com>
2024-06-19 11:01:29 +0200

    Création des components pour le module Video

--- miamfilm_front/src/app/component/video/video.module.ts
+++ miamfilm_front/src/app/component/video/video.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { VideoListComponent } from './video-list/video-list.component';
+import { VideoItemComponent } from './video-item/video-item.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    VideoListComponent,
+    VideoItemComponent
+  ],
   imports: [
     CommonModule
   ]

5de0e9c24e23bf39238dbbdfe32847fa7549845f
MbMyriam <myriam.mb@ynov.com>
2024-06-19 10:59:19 +0200

    Créations des components unitaires

--- miamfilm_front/src/app/app.module.ts
+++ miamfilm_front/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { HeaderComponent } from './component/header/header.component';
+import { CarouselComponent } from './component/carousel/carousel.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { SearchComponent } from './component/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    CarouselComponent,
+    FooterComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,

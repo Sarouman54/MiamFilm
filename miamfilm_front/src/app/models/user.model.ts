1ac1f0f109bba60c906be9123f8818409d560e54
MbMyriam <myriam.mb@ynov.com>
2024-06-19 12:33:25 +0200

    Création du model user

---
+++ miamfilm_front/src/app/models/user.model.ts
@@ -0,0 +1,9 @@
+export class User {
+    id?: string;
+    last_name?: string;
+    first_name?: string;
+    username?: string;
+    email?: string;
+    created_at?: Date;
+    updated_at?: Date;
+}

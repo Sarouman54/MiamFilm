95e3a9676b19b7dac25cc86a8829a4a6204a04d6
MbMyriam <myriam.mb@ynov.com>
2024-06-19 10:52:13 +0200

    Création des component pour le module Authentification

--- miamfilm_front/src/app/component/auth/auth.module.ts
+++ miamfilm_front/src/app/component/auth/auth.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { SignOutComponent } from './sign-out/sign-out.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    SignOutComponent,
+    ForgetPasswordComponent
+  ],
   imports: [
     CommonModule
   ]

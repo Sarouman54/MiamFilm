070dabfa1fa0edc6e7d68db97b4d692a302715cd
MbMyriam <myriam.mb@ynov.com>
2024-06-20 14:02:28 +0200

    Ajout model video

--- miamfilm_front/src/app/models/video.model.ts
+++ miamfilm_front/src/app/models/video.model.ts
@@ -1,6 +1,16 @@
+import { Time } from "@angular/common";
+
 export class VideoModel {
     id?: number;
     title?: string;
     released?: Date;
-    
+    director?: string;
+    actors?: string;
+    genre?: string;
+    type?: string;
+    runtime?: Time;
+    poster?: string;
+    box_office?: number;
+    created_at?: Date;
+    updated_at?: Date;
 }

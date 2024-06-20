365719de678e1c8aa1b9e92caefac620e4c5b97c
Ambre Rouillon <ambre.rouillon@gmail.com>
2024-06-20 16:25:55 +0200

    header style

--- miamfilm_front/src/app/app-routing.module.ts
+++ miamfilm_front/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+import { HomeComponent } from './component/home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+
+
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

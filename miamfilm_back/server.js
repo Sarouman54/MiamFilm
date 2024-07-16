815d1b8daf0d390c1688595ffcc144fffbe2ba1e
Sarouman54 <sarahbarrabe@gmail.com>
2024-07-16 21:00:37 +0200

    test(server): add video data test

--- miamfilm_back/server.js
+++ miamfilm_back/server.js
@@ -1,6 +1,7 @@
 const db = require('./models/indexModel');
 const app = require('./app');
 const bcrypt = require('bcrypt')
+const video = require('./services/videoService')
 
 // db.sequelize.sync();
 
@@ -13,6 +14,13 @@ db.instance.sync({force: true}).then(async () => {
     await db.role.create({name: "admin", description: "Le rôle admin a pour fonction de gérer les utilisateurs", created_at: new Date(), updated_at: new Date()});
     await db.role.create({name: "editor", description: "Le rôle editor a pour fonction de gérer les critiques des films et d'associer les recettes à ceux-ci", created_at: new Date(), updated_at: new Date()});
     await db.role.create({name: "user", description: "Le rôle user est accordé aux utilisateurs connectés afin que ceux-ci bénéficient de droits supplémentaires", created_at: new Date(), updated_at: new Date()});
+
+    await video.addVideo("Le Seigneur des anneaux: La communauté de l'anneau");
+    await video.addVideo("La Reine des neiges");
+    await video.addVideo("How to train your dragon");
+    await video.addVideo("Harry Potter et le prince de sang mêlé");
+    await video.addVideo("Vice Versa 2");
+    await video.addVideo("Le comte de Montecristo");
 
 }).catch((e) => {
     console.error(e)
